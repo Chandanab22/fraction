e9105933d295268d1a56201701121026aa579d45
shrayaas <117797136+shrayaas@users.noreply.github.com>
2023-04-20 11:57:38 +0530

    Update fraction.js

--- fraction.js
+++ fraction.js
@@ -1,1 +1,25 @@
-print("hello")
+class Fraction {
+  constructor(numerator, denominator) {
+    this.numerator = numerator;
+    this.denominator = denominator;
+  }
+  
+  subtract(otherFraction) {
+    const commonDenominator = this.denominator * otherFraction.denominator;
+    const numerator1 = this.numerator * otherFraction.denominator;
+    const numerator2 = otherFraction.numerator * this.denominator;
+    const resultNumerator = numerator1 - numerator2;
+    
+    return new Fraction(resultNumerator, commonDenominator);
+  }
+  
+  toString() {
+    return `${this.numerator}/${this.denominator}`;
+  }
+}
+
+const fraction1 = new Fraction(3, 4);
+const fraction2 = new Fraction(1, 4);
+
+const result = fraction1.subtract(fraction2);
+console.log(result.toString());
